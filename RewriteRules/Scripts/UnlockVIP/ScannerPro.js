22c478833e0a7eca8887bea500c7024622ca42bc
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-25 16:55:19 +0800

    Update ScannerPro.js

--- RewriteRules/Scripts/UnlockVIP/ScannerPro.js
+++ RewriteRules/Scripts/UnlockVIP/ScannerPro.js
@@ -2,5 +2,5 @@
 扫描全能王解锁部分高级特权
 */
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"4102329600"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"1672416000"}}};
 $done({body: JSON.stringify(obj)});
