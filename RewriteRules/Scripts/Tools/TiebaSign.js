fe9008f43de3b9315bbf780e9e897a169503b292
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-26 13:49:41 +0800

    Update TiebaSign.js

--- RewriteRules/Scripts/Tools/TiebaSign.js
+++ RewriteRules/Scripts/Tools/TiebaSign.js
@@ -18,7 +18,7 @@ for(var key in data){
     i+=key+'='+ data[key]
 }
 i+='tiebaclient!!!'
-var v1 = hex_md5(encodeURI(i)).toUpperCase();
+var v1 = hex_md5("b'BDUSS=o4Nno4V1N0NVZSZzBtRnphaWoyR09ERmYyWVRUeXd5QjlCTU1QflhFbWN5dDFoSUFBQUFBJCQAAAAAAAAAAAEAAACyQ0odaHVzMTk5MwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJw9tmGcPbZhaFkw=\xe6\x9d\x8e\xe6\xaf\x85tbs=19ef3f4659ef56291639333786timestamp=1653543144601tiebaclient!!!'").toUpperCase();
 data['sign']=v1;
 const myRequest = {
     url: url,
