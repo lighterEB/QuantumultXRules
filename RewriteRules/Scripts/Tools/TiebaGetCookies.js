6b7d2b6a6ce93b64c1807a5c7fcb1efa396b91eb
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-27 11:52:50 +0800

    Create TiebaGetCookies.js

---
+++ RewriteRules/Scripts/Tools/TiebaGetCookies.js
@@ -0,0 +1,39 @@
+function getCookies() {
+    try {
+      if ($request.headers && $request.url.match(/c\.tieba\.baidu\.com/)) {
+        var CookieName = "百度贴吧";
+        var CookieKey = "CookieTB";
+        var CookieValue = $request.headers['Cookie'];
+        var BDUSS = CookieValue.split(';')[4].split('=')[1];
+        if ($prefs.valueForKey(CookieKey)) {
+          if ($prefs.valueForKey(CookieKey) != CookieValue) {
+            var cookie = $prefs.setValueForKey(CookieValue, CookieKey);
+            $prefs.setValueForKey(BDUSS, 'BDUSS');
+            if (!cookie) {
+              $notify("", "", "更新" + CookieName + "Cookie失败 ‼️");
+            } else {
+              $notify("", "", "更新" + CookieName + "Cookie成功 🎉");
+            }
+          }
+        } else {
+          var cookie = $prefs.setValueForKey(CookieValue, CookieKey);
+          $prefs.setValueForKey(BDUSS, 'BDUSS');
+          if (!cookie) {
+            $notify("", "", "首次写入" + CookieName + "Cookie失败 ‼️");
+          } else {
+            $notify("", "", "首次写入" + CookieName + "Cookie成功 🎉");
+          }
+        }
+      } else {
+        $notify("写入Cookie失败", "", "请检查匹配URL或配置内脚本类型 ‼️");
+      }
+    } catch (eor) {
+      $notify("写入Cookie失败", "", "未知错误 ‼️")
+      console.log(JSON.stringify(eor) + "\n" + eor + "\n" + JSON.stringify($request.headers))
+    }
+    $.done();
+  }
+function getTbs() {
+  try {
+  }
+}
