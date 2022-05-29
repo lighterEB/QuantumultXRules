ac4a4cb72840c97a95a8fc8d6594cdd5192e6e44
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-30 02:03:02 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -4,9 +4,18 @@ for (var i=0; i<obj['data'].length; i++){
     var content = JSON.parse(JSON.parse(JSON.stringify(obj['data'][i]['content'])));
     for(var key in content){
         if(key=='insert_ads'){
-            console.log(content[key]);
+            delete content[key]['roll_time_list'];
+            content[key]['has_insert_ads'] = false;
+            content[key]['has_roll_ads'] = false;
+            console.log('干掉视频中广告！');
         }
+        if(key=='ad_label'){
+            content = '';
+            console.log('干掉广告是视频！');
+        }
+    
     }
+    obj['data'][i]['content']=content;
 //     for (var key in obj['data'][i]['content']){
 //         console.log(key);
 //         if (key == 'insert_ads'){
