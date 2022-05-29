0597ae3a421597dca933ea0f6bbf1ed84a1e76e7
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-30 00:36:18 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -1,7 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 for (var i=0; i<obj['data'].length; i++){
-    console.log(JSON.stringify(obj['data'][i]['content']));
+    console.log(typeof(JSON.stringify(obj['data'][i]['content'])));
 //     for (var key in obj['data'][i]['content']){
 //         console.log(key);
 //         if (key == 'insert_ads'){
