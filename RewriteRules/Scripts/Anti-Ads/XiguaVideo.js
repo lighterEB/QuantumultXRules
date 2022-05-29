ef7faa15ed8b9894590af2c15e31ef45aae34de6
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-30 02:12:11 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -1,21 +1,20 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 for (var i=0; i<obj['data'].length; i++){
-    var content = JSON.parse(JSON.parse(JSON.stringify(obj['data'][i]['content'])));
-    for(var key in content){
-        if(key=='insert_ads'){
-            delete content[key]['roll_time_list'];
-            content[key]['has_insert_ads'] = false;
-            content[key]['has_roll_ads'] = false;
-            console.log('干掉视频中广告！');
-        }
-        if(key=='ad_label'){
-            content = '';
-            console.log('干掉广告是视频！');
-        }
-    
+    content = JSON.parse(JSON.parse(JSON.stringify(obj['data'][i]['content'])));
+    if('insert_ads' in content){
+        delete content['insert_ads']['roll_time_list'];
+        content['insert_ads']['has_insert_ads'] = false;
+        content['insert_ads']['has_roll_ads'] = false;
+        console.log('干掉视频中广告！');
+        obj['data'][i]['content']=content;
+        break
     }
-    obj['data'][i]['content']=content;
+    if('ad_label' in content){
+        obj['data'][i]['content']='';
+        console.log('干掉广告视频！');
+        break
+    }
 //     for (var key in obj['data'][i]['content']){
 //         console.log(key);
 //         if (key == 'insert_ads'){
