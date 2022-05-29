1a22c179a76bdcf335a8505699fb4e185744b9d6
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-29 23:55:36 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -1,14 +1,12 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj['data']['settings']['video_ad_config']['byte_ad_tracker_config']['is_enable_monitor'] = false;
-obj['data']['settings']['video_ad_config']['byte_ad_tracker_config']['is_enable'] = false;
-obj['data']['settings']['video_ad_config']['byte_ad_tracker_config']['settings']['c2s']['store_when_offline'] = false;
-obj['data']['settings']['video_ad_config']['byte_ad_tracker_config']['settings']['c2s']['is_enable'] = false;
-obj['data']['settings']['video_ad_config']['video_ad_preload_appstore_skanasyn_enable'] = 0;
-obj['data']['settings']['video_ad_config']['video_ad_deep_link_dialog_intercept_enable'] = 0;
-obj['data']['settings']['video_ad_config']['ad_monitor_config']['ad_visibility_monitor_enable'] = 0;
-obj['data']['settings']['video_ad_config']['ad_force_send_realtime_click_enable'] = 0;
-obj['data']['settings']['xigua_video_player_config']['ad_prerender_enable'] = 0;
-obj['data']['settings']['video_ad_config']['is_enable_byte_ad_tracker'] = 0;
-obj['data']['settings']['xg_comment']['write_comment_placeholder'] = '请勿做键盘侠，如果要喷就使劲喷';
+for (var i=0; i<obj['data'].length; i++){
+    for (var key in obj['data'][i]['content']){
+        console.log(key);
+        if (key == 'insert_ads'){
+              obj['data'][i]['content'][key]['has_insert_ads'] = 'false';
+              obj['data'][i]['content'][key]['has_roll_ads'] = 'false';
+        }
+    }
+}
 $done(JSON.stringify(obj));
