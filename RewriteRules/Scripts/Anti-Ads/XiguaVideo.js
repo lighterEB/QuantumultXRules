276cd969272abe5f23557322dbe0fc8e1b4f743d
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-29 21:05:44 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -1,7 +1,7 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 obj['data']['settings']['video_ad_config'] = ' ';
-if(obj['data']['settings']['video_ad_config'] == ''){
+if(obj['data']['settings']['video_ad_config'] == ' '){
     console.log("成功！");
 }
 
