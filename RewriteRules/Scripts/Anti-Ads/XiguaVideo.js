ef2041efae1249d0f5ee0fcf2b139992a1645960
t0s1x7 <31654864+lighterEB@users.noreply.github.com>
2022-05-30 10:17:21 +0800

    Update XiguaVideo.js

--- RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
+++ RewriteRules/Scripts/Anti-Ads/XiguaVideo.js
@@ -2,18 +2,16 @@ var body = $response.body;
 var obj = JSON.parse(body);
 for (var i=0; i<obj['data'].length; i++){
     content = JSON.parse(JSON.parse(JSON.stringify(obj['data'][i]['content'])));
-    if('insert_ads' in content){
+    if('insert_ads' in content && 'ad_label' not in content){
         delete content['insert_ads']['roll_time_list'];
         content['insert_ads']['has_insert_ads'] = false;
         content['insert_ads']['has_roll_ads'] = false;
         console.log('干掉视频中广告！');
         obj['data'][i]['content']=content;
-    }
-    if('ad_label' in content || 'bury_count' in content || 'ad_id' in content){
-        obj['data'][i]['content']='';
-        console.log('干掉广告视频！');
     }else{
-        console.log(JSON.stringify(content['filter_words']));
+        obj['data'][i]['content']='';
+        console.log('干掉广告视频！);
+        console.log(JSON.stringify(content));
     }
 //     for (var key in obj['data'][i]['content']){
 //         console.log(key);
